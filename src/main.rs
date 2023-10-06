b94525079186ff327867661102f836330ff82f31
Jesse Clark <hello@developerjesse.com>
2023-10-06 15:18:46 +0300

    Put tests back in main.rs file, tech debt for later.

--- src/main.rs
+++ src/main.rs
@@ -102,3 +102,16 @@ fn write_line(length: usize) -> String {
     }
     return return_value;
 }
+
+#[test]
+fn test_increment() {
+    assert_eq!(increment(5), 6);
+    assert_eq!(increment(0), 1);
+}
+
+#[test]
+fn test_format_color() {
+    assert_eq!(format_color("hello", Color::Red), "\x1b[91mhello\x1b[0m");
+    assert_eq!(format_color("hello", Color::Green), "\x1b[92mhello\x1b[0m");
+    assert_eq!(format_color("hello", Color::Yellow), "\x1b[93mhello\x1b[0m");
+}
