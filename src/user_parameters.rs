6e1c1f1e8eab15b1c21de09ffa4d42ce2a8d2620
Jesse Clark <hello@developerjesse.com>
2023-10-25 17:12:05 +0300

    Clean up comments, and add max to the help section.

--- src/user_parameters.rs
+++ src/user_parameters.rs
@@ -4,15 +4,16 @@ pub fn handle_user_parameters(args: Vec<String>) -> u32 {
         match arg.as_str() {
             "--help" | "-h" => {
                 println!("Jesse's Guessing Game!");
-                println!("");
+                println!();
                 println!("  Select a number between 1 and 100");
                 println!("  Too see the range of numbers already guessed, type hint.");
                 println!("  Try to guess the number with the least number of guesses.");
-                println!("");
+                println!();
                 println!("Options:");
-                println!("  -h, --help      Prints this message.");
-                println!("  -v, --version   Gets the version number");
-                println!("");
+                println!("  -h, --help          Prints this message.");
+                println!("  -v, --version       Gets the version number");
+                println!("  --max_number=1000   Sets the max number");
+                println!();
             }
 
             "--version" | "-v" => {
@@ -20,12 +21,10 @@ pub fn handle_user_parameters(args: Vec<String>) -> u32 {
             }
 
             arg if arg.starts_with("--max_number=") => {
-                println!("handing the max number");
                 // splits the contents into an Iterator with two parts
                 // the nth() gets the second part (i.e. 1). Since this 
                 // is not an array, you can't select it with [1]:
                 if let Some(number) = arg.split('=').nth(1) {
-                    println!("it is some number: {}", number);
                     if let Ok(number) = number.parse::<u32>() {
                         max_number = number;
                     } else {
