ea540f334cba29ed4adbb51752e213949b888821
Jesse Clark <hello@developerjesse.com>
2023-10-25 16:11:38 +0300

    Get help menu to display with options

--- src/user_parameters.rs
+++ src/user_parameters.rs
@@ -1,5 +1,24 @@
 pub fn handle_user_parameters(args: Vec<String>) {
-    println!("handling user arguments!");
-    println!("{:?}", args);
-    // let  = env::args().collect();
+    for arg in args.iter() {
+        match arg.as_str() {
+            "--help" | "-h" => {
+                println!("Jesse's Guessing Game!");
+                println!("");
+                println!("  Select a number between 1 and 100");
+                println!("  Too see the range of numbers already guessed, type hint.");
+                println!("  Try to guess the number with the least number of guesses.");
+                println!("");
+                println!("Options:");
+                println!("  -h, --help      Prints this message.");
+                println!("");
+            }
+
+            "--version" | "-v" => {
+                println!("Version: 1.0");
+            }
+
+            // need to handle the default match, in this case, ignore it.
+            _ => {}
+        }
+    }
 }
